00c1e85a6a240aed603a00e4430830e6d33c97e1
suicodave <suico.dave@gmail.com>
2019-09-09 16:11:29 +0800

    added basic testcase setup

---
+++ tests/Feature/LoanTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace davedev\Lending\Tests\Feature;
+
+use davedev\Lending\Tests\TestCase;
+
+class LoanTest extends TestCase
+{
+    /** 
+     * @test
+     */
+    function can_create_loan()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace davedev\Lending\Tests;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{ }
