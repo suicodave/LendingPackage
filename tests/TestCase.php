fbdef01042fbdbf691eff938d76d863f8241ccc2
suicodave <suico.dave@gmail.com>
2019-09-09 17:01:00 +0800

    testcase added service provider

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,15 @@
 
 namespace davedev\Lending\Tests;
 
+use davedev\Lending\LendingServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
-{ }
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            LendingServiceProvider::class
+        ];
+    }
+}
